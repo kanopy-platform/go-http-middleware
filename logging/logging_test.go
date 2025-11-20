f703434ce10632dfe674232b53d6de11c88527cc
Rojan Dinc <rojand94@gmail.com>
2025-11-20 14:37:11 +0100

    Add slog logging middleware (#5)
    * add slog
    
    * update default logger for slog
    
    * add middleware interface
    
    * update tests
    
    * rename test file
    
    * remove interface and update tests
    
    * rename test file
    
    * bump golangci image tag version in ci
    
    * update fakehandler to comply with linter
    
    * Apply suggestion from @dskatz
    
    Co-authored-by: David Katz <david.katz@mongodb.com>
    
    * bump go version to 1.24
    
    * remove add make command from pipeline
    
    * add slog as reviewed license
    
    * bump licensed-go image version
    
    * remove slog from the licensed file
    
    * Update go.mod
    
    Co-authored-by: David Katz <david.katz@mongodb.com>
    
    ---------
    
    Co-authored-by: David Katz <david.katz@mongodb.com>

---
+++ logging/logging_test.go
@@ -0,0 +1,73 @@
+package logging
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func FakeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := fmt.Fprint(w, `{"retval": "done"}`); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestLoggingMiddleware(t *testing.T) {
+	cases := []struct {
+		name              string
+		loggingMiddleware func(writer io.Writer) func(next http.Handler) http.Handler
+	}{
+		{
+			name: "logrusMiddleware",
+			loggingMiddleware: func(writer io.Writer) func(next http.Handler) http.Handler {
+				logger := logrus.New()
+				logger.SetOutput(writer)
+				lr := NewLogrus(WithLogrus(logger))
+				return func(next http.Handler) http.Handler {
+					return lr.Middleware(next)
+				}
+			},
+		},
+		{
+			name: "slogMiddleware",
+			loggingMiddleware: func(writer io.Writer) func(next http.Handler) http.Handler {
+				s := NewSlog(WithSlog(slog.New(slog.NewTextHandler(writer, nil))))
+				return func(next http.Handler) http.Handler {
+					return s.Middleware(next)
+				}
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", "/some-path", nil)
+			assert.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(FakeHandler)
+
+			// setup logrus
+			var capture bytes.Buffer
+			writer := bufio.NewWriter(&capture)
+
+			// assert middleware
+			tc.loggingMiddleware(writer)(handler).ServeHTTP(rr, req)
+
+			assert.NoError(t, writer.Flush())
+			assert.Contains(t, capture.String(), "method=GET path=/some-path proto=HTTP")
+			assert.Equal(t, http.StatusOK, rr.Code)
+		})
+	}
+}
